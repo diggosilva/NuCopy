7ea07a9bfab20e24d875e72c40c7270a7f8f3253
Diggo Silva <rdiggosilva@gmail.com>
2025-10-31 10:59:16 -0300

    Created custom cell HeaderBottomCell

--- NuCopy/App/Feed/HeaderBottomCell.swift
+++ NuCopy/App/Feed/HeaderBottomCell.swift
@@ -5,4 +5,43 @@
 //  Created by Diggo Silva on 31/10/25.
 //
 
-import Foundation
+import UIKit
+
+final class HeaderBottomCell: UICollectionViewCell {
+    
+    static let identifier: String = "HeaderBottomCell"
+    
+    lazy var grettingLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 24, weight: .semibold)
+        label.textColor = .white
+        label.text = "Olá, John"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        contentView.addSubview(grettingLabel)
+        contentView.backgroundColor = .roxinho
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            grettingLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            grettingLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            grettingLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+        ])
+    }
+}
