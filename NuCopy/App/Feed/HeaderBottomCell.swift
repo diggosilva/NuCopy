34a47cbb0f86502774f2678017f420ec21511324
Diggo Silva <rdiggosilva@gmail.com>
2025-10-31 10:54:44 -0300

    Created Extensions file

---
+++ NuCopy/App/Feed/HeaderBottomCell.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderBottomCell.swift
+//  NuCopy
+//
+//  Created by Diggo Silva on 31/10/25.
+//
+
+import Foundation
---
+++ NuCopy/App/Feed/HeaderTopCell.swift
@@ -0,0 +1,8 @@
+//
+//  HeaderTopCell.swift
+//  NuCopy
+//
+//  Created by Diggo Silva on 31/10/25.
+//
+
+import Foundation
