63ce3dcae3363a7ada8a6f9b913330b8bc008e0d
Diggo Silva <rdiggosilva@gmail.com>
2025-10-31 08:59:38 -0300

    Created HomeView file and connected with HomeViewController

--- NuCopy/App/Feed/HomeViewController.swift
+++ NuCopy/App/Feed/HomeViewController.swift
@@ -8,21 +8,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private let homeView = HomeView()
+    
+    override func loadView() {
+        view = homeView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.5099446177, green: 0.03479245678, blue: 0.8188650012, alpha: 1)
+        configureDelegatesAndDataSources()
+    }
+    
+    private func configureDelegatesAndDataSources() {
+        homeView.collectionView.delegate = self
+        homeView.collectionView.dataSource = self
     }
 
 }
 
-extension HomeViewcontroller: UICollectionViewDelegate, UICollectionViewDataSource {
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        50
+        return 50
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
     }
 }
 
