a85649d4037180b04e3e211ff59c470ad7cd98af
Diggo Silva <rdiggosilva@gmail.com>
2025-10-31 10:58:55 -0300

    Created custom cell HeaderTopCell

--- NuCopy/App/Feed/HeaderTopCell.swift
+++ NuCopy/App/Feed/HeaderTopCell.swift
@@ -5,4 +5,82 @@
 //  Created by Diggo Silva on 31/10/25.
 //
 
-import Foundation
+import UIKit
+
+final class HeaderTopCell: UICollectionViewCell {
+    
+    static let identifier: String = "HeaderTopCell"
+    
+    lazy var photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.contentMode = .scaleAspectFit
+        iv.image = UIImage(systemName: "person.crop.circle")
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    lazy var eyeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "eye"), for: .normal)
+        return button
+    }()
+    
+    lazy var helpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "questionmark.circle"), for: .normal)
+        return button
+    }()
+    
+    lazy var verifyButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "checkmark.shield"), for: .normal)
+        return button
+    }()
+    
+    lazy var HStack: UIStackView = {
+        let sv = UIStackView(arrangedSubviews: [eyeButton, helpButton, verifyButton])
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.axis = .horizontal
+        sv.distribution = .fillEqually
+        sv.spacing = 16
+        return sv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        contentView.addSubview(photoImageView)
+        contentView.addSubview(HStack)
+        contentView.backgroundColor = .roxinho
+        photoImageView.tintColor = .white
+        HStack.tintColor = .white
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            photoImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            photoImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            photoImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 24),
+            photoImageView.widthAnchor.constraint(equalToConstant: 50),
+            photoImageView.heightAnchor.constraint(equalToConstant: 50),
+            
+            HStack.centerYAnchor.constraint(equalTo: photoImageView.centerYAnchor),
+            HStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -24),
+            HStack.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+}
