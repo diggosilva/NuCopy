fc442a10aca32bd09659ea44ffccac6558794478
Diggo Silva <rdiggosilva@gmail.com>
2025-11-02 09:08:59 -0300

    Created AccountCellModel object

--- NuCopy/App/Model/AccountCellModel.swift
+++ NuCopy/App/Model/AccountCellModel.swift
@@ -5,4 +5,23 @@
 //  Created by Diggo Silva on 02/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct AccountCellModel: CellModelProtocol {
+    
+    let titleLabel: String
+    let chevronImageView: UIImage?
+    let descriptionLabel: String
+    let buttonLabel: String
+    
+    var cellIdentifier: String { AccountCell.identifier }
+    var cellHeight: CGFloat { 60 }
+    
+    func configure(_ cell: UITableViewCell) {
+        guard let cell = cell as? AccountCell else { return }
+        cell.titleLabel.text = titleLabel
+        cell.chevronImageView.image = chevronImageView
+        cell.descriptionLabel.text = descriptionLabel
+        cell.buttonLabel.text = buttonLabel
+    }
+}
