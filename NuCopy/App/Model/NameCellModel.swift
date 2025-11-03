a4c9a1676ac692e4fcc00709feca1ca53b607f9c
Diggo Silva <rdiggosilva@gmail.com>
2025-11-03 09:25:49 -0300

    Refactored configure method to receive delegate

--- NuCopy/App/Model/NameCellModel.swift
+++ NuCopy/App/Model/NameCellModel.swift
@@ -13,7 +13,7 @@ struct NameCellModel: CellModelProtocol {
     var cellIdentifier: String { NameCell.identifier }
     var cellHeight: CGFloat { 60 }
     
-    func configure(_ cell: UITableViewCell) {
+    func configure(_ cell: UITableViewCell, delegate: CellCommonActionsDelegate?) {
         guard let cell = cell as? NameCell else { return }
         cell.grettingLabel.text = "Olá, \(username)"
     }
