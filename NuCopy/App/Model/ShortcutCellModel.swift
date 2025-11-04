871c94b28c293a765709cab43884c7fd4b2d07a2
Diggo Silva <rdiggosilva@gmail.com>
2025-11-04 08:46:18 -0300

    Created ShortcutItemModel object

--- NuCopy/App/Model/ShortcutCellModel.swift
+++ NuCopy/App/Model/ShortcutCellModel.swift
@@ -5,4 +5,22 @@
 //  Created by Diggo Silva on 03/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct ShortcutItemModel {
+    let imageName: String
+    let title: String
+}
+
+struct ShortcutCellModel: CellModelProtocol {
+    
+    let shortcuts: [ShortcutItemModel]
+    
+    var cellIdentifier: String { ShortcutTableCell.identifier }
+    var cellHeight: CGFloat { 100 }
+    
+    func configure(_ cell: UITableViewCell, delegate: (any CellCommonActionsDelegate)?) {
+        guard let cell = cell as? ShortcutTableCell else { return }
+        cell.configure(with: shortcuts)
+    }
+}
