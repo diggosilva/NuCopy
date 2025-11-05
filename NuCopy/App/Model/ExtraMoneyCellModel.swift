9b7ae15b64a8ad611be7bc0a3a6dffb943118465
Diggo Silva <rdiggosilva@gmail.com>
2025-11-05 11:04:09 -0300

    Created ExtraMoneyCellModel object

--- NuCopy/App/Model/ExtraMoneyCellModel.swift
+++ NuCopy/App/Model/ExtraMoneyCellModel.swift
@@ -5,4 +5,23 @@
 //  Created by Diggo Silva on 05/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct ExtraMoneyCellModel: CellModelProtocol {
+    
+    let moneyImageSystemName: String
+    let titleLabel: String
+    let descriptionLabel: String
+    let chevronSystemName: String
+    
+    var cellIdentifier: String { ExtraMoneyCell.identifier }
+    var cellHeight: CGFloat { 60 }
+    
+    func configure(_ cell: UITableViewCell, delegate: (any CellCommonActionsDelegate)?) {
+        guard let cell = cell as? ExtraMoneyCell else { return }
+        cell.moneyImageView.image = UIImage(systemName: moneyImageSystemName)
+        cell.titleLabel.text = titleLabel
+        cell.descriptionLabel.text = descriptionLabel
+        cell.chevronImageView.image = UIImage(systemName: chevronSystemName)
+    }
+}
