17d896bf615ecafd7c5864ff1188fadfdc7fa48a
Diggo Silva <rdiggosilva@gmail.com>
2025-11-06 08:40:44 -0300

    Removed configure method

--- NuCopy/App/Model/HeaderCellModel.swift
+++ NuCopy/App/Model/HeaderCellModel.swift
@@ -7,22 +7,10 @@
 
 import UIKit
 
-protocol CellModelProtocol {
-    var cellIdentifier: String { get }
-    var cellHeight: CGFloat { get }
-    func configure(_ cell: UITableViewCell, delegate: CellCommonActionsDelegate?)
-}
-
-struct HeaderCellModel: CellModelProtocol {
+struct HeaderCellModel {
     
     let imageSystemName: String
     
     var cellIdentifier: String { HeaderCell.identifier }
     var cellHeight: CGFloat { 80 }
-    
-    func configure(_ cell: UITableViewCell, delegate: CellCommonActionsDelegate?) {
-        guard let cell = cell as? HeaderCell else { return }
-        cell.photoImageView.image = UIImage(systemName: imageSystemName)
-        cell.delegate = delegate
-    }
 }
