917e06f51b214c12922c4707280bc92394a2ed53
Diggo Silva <rdiggosilva@gmail.com>
2025-11-07 08:36:11 -0300

    Created InviteCardCellModel

--- NuCopy/App/Model/InviteCardCellModel.swift
+++ NuCopy/App/Model/InviteCardCellModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct InviteItemModel {
+    let title: String
+    let iconSystemName: String
+}
+
+struct InviteCardCellModel {
+    
+    var inviteItems: [InviteItemModel]
+    
+    var cellIdentifier: String { InviteCardTableCell.identifier }
+    var celHeight: CGFloat { 150 }
+}
