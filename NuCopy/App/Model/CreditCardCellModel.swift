58ae218364371d2214792af82b71173209ac2171
Diggo Silva <rdiggosilva@gmail.com>
2025-11-09 09:36:23 -0300

    Created CreditCardCellModel object

--- NuCopy/App/Model/CreditCardCellModel.swift
+++ NuCopy/App/Model/CreditCardCellModel.swift
@@ -5,4 +5,16 @@
 //  Created by Diggo Silva on 08/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct CreditCardCellModel {
+    
+    let titleLabel: String
+    let chevronSystemName: String
+    let currentInvoiceLabel: String
+    let invoiceAmountLabel: String
+    let availableLimitLabel: String
+    
+    var cellIdentifier: String { CreditCardCell.identifier }
+    var cellHeight: CGFloat { 150 }
+}
