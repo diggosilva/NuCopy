81027428dc10e65c64110600609781a8a908a55a
Diggo Silva <rdiggosilva@gmail.com>
2025-11-10 10:17:32 -0300

    Created MyCardsCellModel object

--- NuCopy/App/Model/MyCardsCellModel.swift
+++ NuCopy/App/Model/MyCardsCellModel.swift
@@ -5,4 +5,13 @@
 //  Created by Diggo Silva on 09/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct MyCardsCellModel {
+    
+    let iconSystemName: String
+    let title: String
+    
+    var cellIdentifier: String { MyCardsCell.identifier }
+    var cellHeight: CGFloat { 60 }
+}
