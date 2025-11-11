6d0a9d5d841d95a16e9b975035e73cd9259856c7
Diggo Silva <rdiggosilva@gmail.com>
2025-11-11 09:30:22 -0300

    Created LoanCellModel object

--- NuCopy/App/Model/LoanCellModel.swift
+++ NuCopy/App/Model/LoanCellModel.swift
@@ -5,4 +5,15 @@
 //  Created by Diggo Silva on 10/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct LoanCellModel {
+    
+    let titleLabel: String
+    let chevronSystemName: String
+    let loanTitleLabel: String
+    let loanAmountLabel: String
+    
+    var cellIdentifier: String { LoanCell.identifier }
+    var cellHeight: CGFloat { 100 }
+}
