db32694fe7588df14965809c879a289ef4dd761c
Diggo Silva <rdiggosilva@gmail.com>
2025-11-12 07:44:39 -0300

    Created PaymentAssistantCellModel object

--- NuCopy/App/Model/PaymentAssistantCellModel.swift
+++ NuCopy/App/Model/PaymentAssistantCellModel.swift
@@ -5,4 +5,15 @@
 //  Created by Diggo Silva on 11/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct PaymentAssistantCellModel {
+    
+    let titleLabel: String
+    let chevronSystemName: String
+    let descriptionLabel: String
+    let buttonLabel: String
+    
+    var cellIdentifier: String { PaymentAssistantCell.identifier }
+    var cellHeight: CGFloat { 100 }
+}
