f142c90ffae168a84dc9c5edb27e5587ee130c29
Diggo Silva <rdiggosilva@gmail.com>
2025-11-13 07:52:09 -0300

    Created DiscoverCardCellModel object

---
+++ NuCopy/App/Scenes/Home/CustomCells/DiscoverCardCollectionCell.swift
@@ -0,0 +1,8 @@
+//
+//  DiscoverCardCollectionCell.swift
+//  NuCopy
+//
+//  Created by Diggo Silva on 12/11/25.
+//
+
+import Foundation
---
+++ NuCopy/App/Scenes/Home/CustomCells/DiscoverCardTableCell.swift
@@ -0,0 +1,8 @@
+//
+//  DiscoverCardTableCell.swift
+//  NuCopy
+//
+//  Created by Diggo Silva on 12/11/25.
+//
+
+import Foundation
