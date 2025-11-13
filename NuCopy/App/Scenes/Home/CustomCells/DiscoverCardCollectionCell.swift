f36cf67e1e18640186e6b6c0e0623a460e94b461
Diggo Silva <rdiggosilva@gmail.com>
2025-11-13 07:53:45 -0300

    Created DiscoverCardCollectionCell custom

--- NuCopy/App/Scenes/Home/CustomCells/DiscoverCardCollectionCell.swift
+++ NuCopy/App/Scenes/Home/CustomCells/DiscoverCardCollectionCell.swift
@@ -5,4 +5,102 @@
 //  Created by Diggo Silva on 12/11/25.
 //
 
-import Foundation
+import UIKit
+
+final class DiscoverCardCollectionCell: UICollectionViewCell {
+    
+    static let identifier: String = "DiscoverCardCollectionCell"
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        imageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        return imageView
+    }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .subheadline)
+        label.textColor = .secondaryLabel
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var buttonLabel: UILabel = {
+        let label = PaddingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .preferredFont(forTextStyle: .headline)
+        label.textColor = .white
+        label.backgroundColor = .roxinho
+        label.layer.cornerRadius = 18
+        label.clipsToBounds = true
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    private func setupView() {
+        setHierarchy()
+        setConstraints()
+        backgroundColor = .systemGray6
+    }
+    
+    private func setHierarchy() {
+        addSubviews(imageView, titleLabel, descriptionLabel, buttonLabel)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 120),
+            
+            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 16),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            
+            buttonLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            buttonLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8),
+        ])
+    }
+    
+    func configure(with model: DiscoverCardItemModel) {
+        imageView.image = UIImage(named: model.namedImage)
+        titleLabel.text = model.title
+        descriptionLabel.text = model.description
+        buttonLabel.text = model.titleButton
+        
+        self.layer.cornerRadius = 10
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            UIView.animate(withDuration: 0.2) {
+                self.backgroundColor = self.isHighlighted ? .systemGray5 : .systemGray6
+                self.imageView.alpha = self.isHighlighted ? 0.5 : 1
+            }
+        }
+    }
+}
