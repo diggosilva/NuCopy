771dc81fb10e0c9443cef2b89ed4cd5a5893eda1
Diggo Silva <rdiggosilva@gmail.com>
2025-11-14 09:14:05 -0300

    Created RateScreenCellModel object

--- NuCopy/App/Model/RateScreenCellModel.swift
+++ NuCopy/App/Model/RateScreenCellModel.swift
@@ -5,4 +5,13 @@
 //  Created by Diggo Silva on 13/11/25.
 //
 
-import Foundation
+import UIKit
+
+struct RateScreenCellModel {
+    
+    let heartIconSystemName: String
+    let title: String
+    
+    var cellIdentifier: String { RateScreenCell.identifier }
+    var cellHeight: CGFloat { 60 }
+}
