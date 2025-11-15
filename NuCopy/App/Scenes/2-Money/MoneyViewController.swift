0fd1898e3207fea1b3876d027ffd4b4319ca74de
Diggo Silva <rdiggosilva@gmail.com>
2025-11-15 08:00:59 -0300

    Created MoneyViewController just for TabBar

--- NuCopy/App/Scenes/2-Money/MoneyViewController.swift
+++ NuCopy/App/Scenes/2-Money/MoneyViewController.swift
@@ -5,4 +5,12 @@
 //  Created by Diggo Silva on 14/11/25.
 //
 
-import Foundation
+import UIKit
+
+class MoneyViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+}
