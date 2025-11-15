927964b8c9857b5347a51962eac1b88b79c4bcd8
Diggo Silva <rdiggosilva@gmail.com>
2025-11-15 08:01:15 -0300

    Created MobileViewController just for TabBar

--- NuCopy/App/Scenes/3-Mobile/MobileViewController.swift
+++ NuCopy/App/Scenes/3-Mobile/MobileViewController.swift
@@ -5,4 +5,12 @@
 //  Created by Diggo Silva on 14/11/25.
 //
 
-import Foundation
+import UIKit
+
+class MobileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+}
