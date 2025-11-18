ca46d10c2056c22ffc845ddad7fadb39b2ebe695
Diggo Silva <rdiggosilva@gmail.com>
2025-11-18 09:37:43 -0300

    Created extensions from HomeCell to handle with cellForRowAt and didSelect methods

--- NuCopy/App/Scenes/1-Home/Model/HomeCell.swift
+++ NuCopy/App/Scenes/1-Home/Model/HomeCell.swift
@@ -40,3 +40,80 @@ enum HomeCell {
         }
     }
 }
+
+extension HomeCell {
+    func configure(cell: UITableViewCell) {
+        switch self {
+        case .header(let model):
+            (cell as? HeaderCell)?.configure(model: model)
+            
+        case .name(let model):
+            (cell as? NameCell)?.configure(model: model)
+            
+        case .account(let model):
+            (cell as? AccountCell)?.configure(model: model)
+            
+        case .shortcuts(let model):
+            (cell as? ShortcutTableCell)?.configure(shortcuts: model.shortcuts)
+            
+        case .extraMoney(let model):
+            (cell as? ExtraMoneyCell)?.configure(model: model)
+            
+        case .invite(let model):
+            (cell as? InviteCardTableCell)?.configure(items: model.inviteItems)
+            
+        case .divider(let model):
+            (cell as? DividerCell)?.configure(model: model)
+            
+        case .creditCard(let model):
+            (cell as? CreditCardCell)?.configure(model: model)
+            
+        case .myCards(let model):
+            (cell as? MyCardsCell)?.configure(model: model)
+            
+        case .loan(let model):
+            (cell as? LoanCell)?.configure(model: model)
+            
+        case .paymentAssistent(let model):
+            (cell as? PaymentAssistantCell)?.configure(model: model)
+            
+        case .discover(let model):
+            (cell as? DiscoverCardTableCell)?.configure(model: model)
+            
+        case .rateScreen(let model):
+            (cell as? RateScreenCell)?.configure(model: model)
+        }
+    }
+}
+
+extension HomeCell {
+    func attachDelegates(to cell: UITableViewCell, controller: HomeViewTableController) {
+        switch self {
+        case .header: (cell as? HeaderCell)?.delegate = controller
+        case .shortcuts: (cell as? ShortcutTableCell)?.delegate = controller
+        case .invite: (cell as? InviteCardTableCell)?.delegate = controller
+        case .discover: (cell as? DiscoverCardTableCell)?.delegate = controller
+        default: break
+        }
+    }
+}
+
+extension HomeCell {
+    func didSelect() {
+        switch self {
+        case .header: print("Header tapped")
+        case .name: print("Name tapped")
+        case .account: print("Account tapped")
+        case .shortcuts: print("Shortcuts tapped")
+        case .extraMoney: print("Extra Money tapped")
+        case .invite: print("Invite tapped")
+        case .divider: print("Divider tapped")
+        case .creditCard: print("Credit Card tapped")
+        case .myCards: print("My Cards tapped")
+        case .loan: print("Loan tapped")
+        case .paymentAssistent: print("Payment Assistent tapped")
+        case .discover: print("Discover tapped")
+        case .rateScreen: print("Rate Screen tapped")
+        }
+    }
+}
