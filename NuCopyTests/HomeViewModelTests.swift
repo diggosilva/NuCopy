dc3a65281dd3e5992902c7786cef463a856dddf6
Diggo Silva <rdiggosilva@gmail.com>
2025-11-19 08:38:49 -0300

    Xcode configuration

---
+++ NuCopyTests/HomeViewModelTests.swift
@@ -0,0 +1,77 @@
+//
+//  NuCopyTests.swift
+//  NuCopyTests
+//
+//  Created by Diggo Silva on 19/11/25.
+//
+
+import XCTest
+@testable import NuCopy
+
+class HomeViewModelTests: XCTestCase {
+    
+    var viewModel: HomeViewModel!
+    
+    override func setUp() {
+        super.setUp()
+        viewModel = HomeViewModel()
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+    
+    func testNumberOfRowsShouldReturnCorrectCount() {
+        XCTAssertEqual(viewModel.numberOfRows(), 16, "O número de células esperadas mudou.")
+    }
+    
+    func testFirstCellShouldBeHeader() {
+        let cell = viewModel.cellModelForRow(at: 0)
+        
+        switch cell {
+        case .header(_):
+            XCTAssertEqual(cell.cellIdentifier, "HeaderCell", "No identifier sempre uso o próprio nome da célula. Verifique.")
+            
+        default:
+            XCTFail("A primeira célula não é do tipo HeaderCell.")
+        }
+    }
+    
+    func testSecondCellShouldBeHeader() {
+        let cell = viewModel.cellModelForRow(at: 1)
+        
+        switch cell {
+        case .name(let model):
+            XCTAssertEqual(model.username, "John", "O nome de usuário deveria ser 'John'.")
+            
+        default:
+            XCTFail("A segunda célula não é do tipo NameCell.")
+        }
+    }
+    
+    func testShortcutsCellShouldContainsFourItems() {
+        let cell = viewModel.cellModelForRow(at: 3)
+        
+        switch cell {
+        case .shortcuts(let model):
+            XCTAssertEqual(model.shortcuts.count, 4)
+            
+        default:
+            XCTFail("Esperado .shortcuts na posição 3")
+        }
+    }
+    
+    func testDiscoverCellShouldContainsEightItems() {
+        let discoverIndex = 14
+        let cell = viewModel.cellModelForRow(at: discoverIndex)
+        
+        switch cell {
+        case .discover(let model):
+            XCTAssertEqual(model.items.count, 8)
+            
+        default:
+            XCTFail("Esperado .discover na posição 14")
+        }
+    }
+}
